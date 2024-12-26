1f2de7bc2bff038fd492b202afd5a62de4a873d8
E-DeBusk <debusk.elijah@gmail.com>
2024-12-25 21:15:08 -0800

    Started panel.js using a draggable guide online

--- panel/components/panel.js
+++ panel/components/panel.js
@@ -1,3 +1,25 @@
 class Panel {
-    
+    constructor(children, position, width, height) {
+        this.children = children
+        this.position = position
+        this.offset = createVector(0, 0)
+        this.width = width
+        this.height = min(height, 550)
+        this.dragging = false
+    }
+
+    update() {
+        if(this.dragging) {
+            this.position.x = mouseX + this.offset.x
+            this.position.y = mouseY + this.offset.y
+        }
+    }
+
+    draw() {
+        
+    }
+
+    within_dragpad() {
+        return (mouseX > this.position.x && mouseY > this.position.y && mouseX < this.position.x + width && mouseY < 20)
+    }
 }
