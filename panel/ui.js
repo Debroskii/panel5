fd1a9321b72645dd3bbf25b8cc91fa64d7f0692f
E-DeBusk <debusk.elijah@gmail.com>
2024-12-26 11:49:14 -0800

    Added panel support and update()

--- panel/ui.js
+++ panel/ui.js
@@ -10,5 +10,16 @@ class UI {
 
         UI.icon_bar.add(new IconButton("assets/icon/refresh.png", () => {return}, 20, 20, "Hehehe"))
         ui.child(UI.icon_bar.create())
+
+        UI.panels.push(Panel.default())
+        for(const panel of UI.panels) {
+            ui.child(panel.create())
+        }
+    }
+
+    static update() {
+        for(const panel of UI.panels) {
+            panel.update()
+        }
     }
 }
