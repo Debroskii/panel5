28700ce5d9a464ec6b4bdf4be43d7170d7854fd1
E-DeBusk <debusk.elijah@gmail.com>
2024-12-25 20:50:08 -0800

    Added a UI wrapper

--- panel/ui.js
+++ panel/ui.js
@@ -1,4 +1,14 @@
 class UI {
-    static bar = null
+    static icon_bar = new IconBar()
     static panels = []
+
+    static create() {
+        let ui = createDiv("")
+        ui.id('ui')
+        ui.style("width", DIMENSIONS[0] + "px")
+        ui.style("height", DIMENSIONS[1] + "px")
+
+        UI.icon_bar.add(new IconButton("assets/icon/refresh.png", () => {return}, 20, 20, "Hehehe"))
+        ui.child(UI.icon_bar.create())
+    }
 }
