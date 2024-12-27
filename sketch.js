8bc348c8977ff8c1a5564cac8b9ee0df2320af05
E-DeBusk <debusk.elijah@gmail.com>
2024-12-27 14:08:17 -0800

    Added a test registry and an ellipse to test modification on

--- sketch.js
+++ sketch.js
@@ -1,8 +1,22 @@
 let DIMENSIONS
 
+let testRegistry
+
 function setup() {
   DIMENSIONS = [windowWidth, windowHeight]
   createCanvas(DIMENSIONS[0], DIMENSIONS[1]);
+
+  testRegistry = new Registry("testreg")
+  testRegistry.addNumber("x", 500, "Circle X Position", true)
+  testRegistry.addNumber("y", 500, "Circle Y Position", true)
+  testRegistry.addBoolean("show", true, "Show Circle", true)
+  testRegistry.addColor("fill_color", color(255, 255, 255, 10), "Circle Color", true)
+
+  GlobalRegistry.addRegistry(testRegistry)
+
+  let panel = new Panel(createVector(500, 500), 200, 300, "Test Panel", testRegistry)
+
+  UI.panels.push(panel)
   UI.create()
 }
 
@@ -11,6 +25,12 @@ function draw() {
   UI.update();
 
   noStroke()
+  if(testRegistry.get("show")) {
+    fill(testRegistry.get("fill_color"))
+  } else {
+    fill(0, 0)
+  }
+  ellipse(testRegistry.get("x"), testRegistry.get("y"), 100, 100)
 }
 
 function keyPressed() {
