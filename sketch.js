b280c5041d5b4448921ca8bb2bfe39bd31e7dac8
E-DeBusk <debusk.elijah@gmail.com>
2024-12-27 11:36:35 -0800

    Removed the previous registry test code

--- sketch.js
+++ sketch.js
@@ -1,26 +1,16 @@
 let DIMENSIONS
 
-let panel
-var radius = 100
-
 function setup() {
   DIMENSIONS = [windowWidth, windowHeight]
   createCanvas(DIMENSIONS[0], DIMENSIONS[1]);
   UI.create()
-
-  panel = Panel.default_at(createVector(DIMENSIONS[0] * 0.75, DIMENSIONS[1] * 0.75))
-  panel.registry.register("radius", radius, RegistryEntryType.NUMBER)
-
-  UI.panels.push(panel)
 }
 
 function draw() {
   background(241);
   UI.update();
 
-  radius = panel.registry.get("radius")
   noStroke()
-  ellipse(500, 300, radius, radius)
 }
 
 function keyPressed() {
