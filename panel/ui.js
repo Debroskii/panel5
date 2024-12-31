10dcd627bc2930d3bc0aa2599ea9ea95b13dc29c
E-DeBusk <debusk.elijah@gmail.com>
2024-12-30 20:17:56 -0800

    something, idk

--- panel/ui.js
+++ panel/ui.js
@@ -1,18 +1,26 @@
 class UI {
     static icon_bar = new IconBar()
     static panels = []
+    static context_menu = new ContextMenu()
 
     static add_panel(panel) {
         UI.panels.push(panel)
     }
 
-    static create() {
+    static init() {
+        document.addEventListener('contextmenu', function(e) {
+            alert("You've tried to open context menu"); //here you draw your own menu
+            e.preventDefault();
+        }, false);
+
+        // UI Creation
         let ui = createDiv("")
         ui.id('ui')
         ui.style("width", DIMENSIONS[0] + "px")
         ui.style("height", DIMENSIONS[1] + "px")
 
         ui.child(UI.icon_bar.create())
+        ui.child(UI.context_menu.create())
 
         UI.panels.push(Panel.default())
         for(const panel of UI.panels) {
