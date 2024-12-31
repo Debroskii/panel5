2b106a03880664afd3649708eefd4ff2d10c660b
E-DeBusk <debusk.elijah@gmail.com>
2024-12-30 20:19:08 -0800

    Created a UI wrapper for panel5 rev2

---
+++ panel5/ui.js
@@ -0,0 +1,43 @@
+class UI {
+    static panels = []
+    static DOMRoot
+
+    static initialize() {
+        UI.DOMRoot = createDiv("").id("UIRoot")
+        UI.DOMRoot.style("width", windowWidth + "px").style("height", windowHeight + "px")
+        for(const panel of UI.panels) {
+            UI.DOMRoot.child(panel.element)
+        }
+    }   
+
+    static addPanel(panel) {
+        UI.panels.push(panel)
+        UI.DOMRoot.child(panel.element)
+    }
+
+    static removePanel(panel) {
+        UI.panels.splice(UI.panels.indexOf(panel), 1)
+    }
+
+    static updatePanels() {
+        for(const panel of UI.panels) {
+            panel.update()
+        }
+    }
+
+    static handleMousePress() {
+        if(mouseButton === LEFT) {
+            for(const panel of UI.panels) {
+                panel.pressed()
+            }
+        }
+    }
+
+    static handleMouseRelease() {
+        if(mouseButton === LEFT) {
+            for(const panel of UI.panels) {
+                panel.released()
+            }
+        }
+    }
+}
