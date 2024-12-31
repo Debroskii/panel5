67b7ce90d7da398139f28aaf171879a50a7f7142
E-DeBusk <debusk.elijah@gmail.com>
2024-12-30 22:49:37 -0800

    made some testing changes

--- sketch.js
+++ sketch.js
@@ -10,9 +10,9 @@ function setup() {
   test_registry = new Registry("test_registry")
   test_registry.registerString("str", "hehehe")
   test_registry.registerNumber("num", 0.0)
-  test_registry.registerBoolean("bool", true)
+  test_registry.registerBoolean("bool", false)
   test_registry.registerColor("color", color(255, 0, 255))
-  test_registry.registerDropdown("dropdown", "a", ["a", "b", "c"])
+  test_registry.registerDropdown("dropdown", "a", ["a", "b", "c"], "Yes...")
 
 
   GlobalRegistry.addRegistry(test_registry)
