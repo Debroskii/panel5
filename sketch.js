33fde67f16557bab341085795479a5b4e0ab44fc
E-DeBusk <debusk.elijah@gmail.com>
2025-01-02 09:21:08 -0800

    Just called frameRate to set the frame rate to 60

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ function setup() {
     e.preventDefault();
   }, false);
   createCanvas(windowWidth, windowHeight);
+  frameRate(60);
 
   test_registry = new Registry("test_registry")
   test_registry.registerString("str", "hehehe")
@@ -19,6 +20,7 @@ function setup() {
 
   UI.initialize()
   UI.addPanel(new RegistryPanel(test_registry, createVector(400, 400), createVector(150, 250), "TEST PANEL"))
+  UI.addPanel(new Panel("r", createVector(100, 100), createVector(200, 200), "ANOTHER TEST PANEL"))
 }
 
 function draw() {
